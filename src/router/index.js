adef90cecbf5c8b81469cde318db279ca1dd8374
ganapati3 <124158856+ganapati3@users.noreply.github.com>
2024-12-08 22:08:08 +0530

    updated: routing to hashed routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
 import UsersView from '@/views/UserPage.vue';
 import RolesView from '@/views/RolesPage.vue';
@@ -12,6 +12,6 @@ const routes = [
 ];
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
